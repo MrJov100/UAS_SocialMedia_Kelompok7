803297e3c560291e5b7f8e8463b5e75ce8f1e2c0
Jessenericko <jessen.535230006@stu.untar.ac.id>
2024-06-06 11:00:04 +0700

    new2jessen

--- social_media_kelompok7/database/migrations/2024_06_06_035131_create_tanggal_lahir_table.php
+++ social_media_kelompok7/database/migrations/2024_06_06_035131_create_tanggal_lahir_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserTable extends Migration
+class CreateTanggalLahirTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,11 @@ class CreateUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('username', function (Blueprint $table) {
+        Schema::create('tanggal_lahir', function (Blueprint $table) {
             $table->id('userid');
-            $table->string('username');
+            $table->integer('tanggal');
+            $table->integer('bulan');
+            $table->integer('tahun');
             $table->timestamps();
         });
     }
@@ -27,6 +29,6 @@ class CreateUserTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('username');
+        Schema::dropIfExists('tanggal_lahir');
     }
 }
