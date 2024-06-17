1620a6edbb7c0a07289635b20900b26833951537
Jovandi <162522277+MrJov100@users.noreply.github.com>
2024-06-17 16:10:50 +0700

    tambahan Auth

--- social_media_kelompok7/routes/web.php
+++ social_media_kelompok7/routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\LandingController;
-
+use Illuminate\Support\Facades\Auth; 
 use App\Http\Controllers\PostinganController;
 
 
